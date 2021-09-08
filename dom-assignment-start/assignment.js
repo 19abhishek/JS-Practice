a125609e34b44416e8c54e808b655aa514a4ea2d
Abhishek Kumar <abhishek_0819@Abhisheks-MacBook-Air.local>
2021-09-08 22:26:23 +0530

    query selector

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,11 @@
+const task2 = document.getElementById('task-1');
+const task1 = document.querySelectorAll('li')[0];
+const title = document.querySelector('title');
+//const title1 = document.querySelector();
+const h1 = document.querySelector('h1');
+
+console.dir(title);
+task1.style.backgroundColor = 'black';
+task1.style.color = 'white';
+title.textContent = "Hello";
+h1.textContent = 'Assignment completed';
