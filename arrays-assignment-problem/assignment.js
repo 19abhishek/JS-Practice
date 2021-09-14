54d796f7e20cbd995b5dd850d7f8af5d53c63914
Abhishek <abhishek.kum1908@gmail.com>
2021-09-14 13:46:15 +0530

    Assignment completed

--- arrays-assignment-problem/assignment.js
+++ arrays-assignment-problem/assignment.js
@@ -3,7 +3,7 @@ const set = new Set();
 const arr = [];
 
 for(let i=0;i<10;i++){
-    const crr = (Math.random()*50)+1;
+    const crr = parseInt((Math.random()*50)+1);
     if(!set.has(crr)){
         arr.push(crr);
     }else{
@@ -11,14 +11,24 @@ for(let i=0;i<10;i++){
     }
 }
 
-const minMax = (arr) => {
-    
+console.log(arr);
+
+const minMax = (a) => {
+    a.sort((x,y) => {
+        if(x>y){
+            return 1;
+        }else if(x==y){
+            return 0;
+        }else{
+            return -1;
+        }
+    });
+    //a.reverse();
+    return [min, max] = [a[0], a[a.length-1]];
+    //return ans;
 }
 
 
 
-function minMax(arr){
-    arr.sort();
-    
-    return ans;
-}
+const ans = minMax(arr);
+console.log(ans);
